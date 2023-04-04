812846f8db95b40718d946afcecec7bf01b195a3
Veljko <veljkoBitno@gmail.com>
2023-04-04 19:58:15 +0200

    Validation success left

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable import/no-absolute-path */
 /* eslint-disable import/no-unresolved */
-import React from "react";
+import React, { useState } from "react";
 import Arrow from "/assets/images/icon-arrow.svg";
 
 const Form = ({ formData, setFormData, setCheckSubmit }) => {
+  const [dayError, setDayError] = useState("");
+  const [monthError, setMonthError] = useState("");
+  const [yearError, setYearError] = useState("");
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
@@ -14,7 +18,45 @@ const Form = ({ formData, setFormData, setCheckSubmit }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setCheckSubmit(true);
+
+    // Day
+    if (formData.day === "" || formData.day === null) {
+      setDayError("This field is required");
+    } else if (formData.day < 1 || formData.day > 31) {
+      setDayError("Must be a valid day");
+    } else {
+      setDayError("");
+    }
+
+    if (formData.month === "" || formData.month === null) {
+      setMonthError("This field is required");
+    } else if (formData.month < 1 || formData.month > 12) {
+      setMonthError("Must be a valid month");
+    } else {
+      setMonthError("");
+    }
+
+    if (formData.year === "" || formData.year === null) {
+      setYearError("This field is required");
+    } else if (formData.year > new Date().getFullYear()) {
+      setYearError("Must be in the past");
+    } else {
+      setYearError("");
+    }
+
+    const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (
+      formData.year % 400 === 0 ||
+      (formData.year % 100 !== 0 && formData.year % 4 === 0)
+    )
+      monthLength[1] = 29;
+
+    if (formData.day >= monthLength[formData.month - 1]) {
+      setDayError("Must be a valid date");
+      setMonthError("");
+      setYearError("");
+    }
   };
 
   return (
@@ -30,6 +72,7 @@ const Form = ({ formData, setFormData, setCheckSubmit }) => {
           onChange={(e) => handleChange(e)}
           value={formData.day}
         />
+        <div className="error">{dayError}</div>
       </label>
       <label className="form__label" htmlFor="month">
         Month
@@ -42,6 +85,7 @@ const Form = ({ formData, setFormData, setCheckSubmit }) => {
           onChange={(e) => handleChange(e)}
           value={formData.month}
         />
+        <div className="error">{monthError}</div>
       </label>
       <label className="form__label" htmlFor="year">
         Year
@@ -54,6 +98,7 @@ const Form = ({ formData, setFormData, setCheckSubmit }) => {
           onChange={(e) => handleChange(e)}
           value={formData.year}
         />
+        <div className="error">{yearError}</div>
       </label>
       <button className="form__button" type="submit">
         <img src={Arrow} alt="Arrow" />
